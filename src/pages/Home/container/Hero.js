1130a285d52d5d89dc5df98276329bdaf6ac390c
unknown <vimalkuriensam@gmail.com>
2021-07-07 00:48:38 +0530

    grid layout

--- src/pages/Home/container/Hero.js
+++ src/pages/Home/container/Hero.js
@@ -1,6 +1,8 @@
 import React from "react";
 
+import Button from "../../../components/atoms/Button";
 import Title from "../../../components/atoms/Title";
+import Input from "../../../components/atoms/Input";
 
 const Hero = () => {
   return (
@@ -8,6 +10,25 @@ const Hero = () => {
       <div className="home__heroContainer">
         <div>
           <Title variant="pr-35-1">Find Job</Title>
+        </div>
+        <div className="row">
+          <div className="col-3-of-7">
+            <Input
+              variant="1"
+              className="home__heroInput"
+              placeholder="Job title or company"
+            />
+          </div>
+          <div className="col-3-of-7">
+            <Input
+              variant="1"
+              className="home__heroInput"
+              placeholder="Location"
+            />
+          </div>
+          <div className="col-1-of-7">
+            <Button variant="2" icon="Search" content="" />
+          </div>
         </div>
       </div>
     </div>

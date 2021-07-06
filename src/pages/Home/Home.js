b48f51d906a164a49a6ba86d0b4655524ee5803d
unknown <vimalkuriensam@gmail.com>
2021-07-06 23:05:52 +0530

    text component refactor

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,8 @@
 import React from "react";
 
-import Title from "../../components/atoms/Title";
-
 const Home = () => {
   return (
     <div style={{ padding: "3rem" }}>
-      <Title variant="pr-19-1">Test Title</Title>
-      <Title variant="pr-24-1">Test Title</Title>
-      <Title variant="pr-25-1">Test Title</Title>
-      <Title variant="pr-28-1">Test Title</Title>
-      <Title variant="pr-30-1">Test Title</Title>
-      <Title variant="pr-35-1">Test Title</Title>
-      <Title variant="pr-54-1">Test Title</Title>
-      <Title variant="pm-26-1">Test Title</Title>
-      <Title variant="pm-30-1">Test Title</Title>
-      <Title variant="pb-21-1">Test Title</Title>
     </div>
   );
 };

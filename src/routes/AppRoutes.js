c95349db51377b8cd8cad16646a67d95187e0929
unknown <vimalkuriensam@gmail.com>
2021-07-06 07:57:35 +0530

    home page route

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,10 +4,17 @@ import { Redirect, Router, Route, Switch } from "react-router-dom";
 import history from "../utils/history";
 import ScrollTop from "../utils/ScrollTop";
 
+import { Home as HomeView } from "../pages";
+
 const AppRoutes = () => (
   <Router history={history}>
     <ScrollTop />
-    <Switch></Switch>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/home" />
+      </Route>
+      <Route path="/home" component={HomeView} />
+    </Switch>
   </Router>
 );
 

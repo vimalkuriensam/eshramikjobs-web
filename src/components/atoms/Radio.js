36dd9a60a61aa927d49c4cba2f6dc94900fe16d1
unknown <vimalkuriensam@gmail.com>
2021-07-06 11:14:44 +0530

    radio component

---
+++ src/components/atoms/Radio.js
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+
+import Text from "./Text";
+
+const Radio = ({ value, id, name, title, onHandleRadioClick }) => {
+  const radioRef = useRef(null);
+  const onHandleClick = () => onHandleRadioClick({ target: { value: id } });
+
+  return (
+    <div className="form__radio">
+      <input
+        type="radio"
+        checked={value}
+        id={id}
+        name={name}
+        className="form__radio--input"
+        ref={radioRef}
+      />
+      <label
+        className="form__radio--label"
+        htmlFor={id}
+        onClick={onHandleClick}
+      >
+        <div className="form__radio--button">&nbsp;</div>
+        <Text variant="r-14-400-1">{title}</Text>
+      </label>
+    </div>
+  );
+};
+
+export default Radio;

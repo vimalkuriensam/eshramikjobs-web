76632e93898d3d05b761843fa617c877985dbde7
unknown <vimalkuriensam@gmail.com>
2021-07-06 10:39:23 +0530

    ruppee icon

---
+++ src/utils/icons/Ruppee.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Ruppee = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="37.816"
+    viewBox="0 0 24 37.816"
+  >
+    <g id="rupee" transform="translate(-93.525)">
+      <path
+        id="Path_36"
+        data-name="Path 36"
+        d="M117.525,0h-24V4.768h8.464a7.165,7.165,0,0,1,6.743,4.768H93.525V14.3h15.207a7.165,7.165,0,0,1-6.743,4.768H93.525v4.768L107.5,37.816l3.372-3.372-10.6-10.6h1.72A11.941,11.941,0,0,0,113.67,14.3h3.856V9.537H113.67a11.871,11.871,0,0,0-2.15-4.768h6.006Z"
+        fill="#1785a9"
+      />
+    </g>
+  </svg>
+);
+
+export default Ruppee;

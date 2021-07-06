76c729ca5fb5d4059a7a11d2442b29aaecbc422f
unknown <vimalkuriensam@gmail.com>
2021-07-06 10:37:56 +0530

    input component

---
+++ src/components/atoms/Input.js
@@ -0,0 +1,43 @@
+import React, { useRef } from "react";
+import Button from "./Button";
+
+const Input = ({
+  onHandleText,
+  onHandleFile,
+  variant,
+  placeholder = "Enter Text",
+  className,
+  type = "text",
+  ...rest
+}) => {
+  if (type == "file") {
+    const ref = useRef();
+    return (
+      <div>
+        <Button
+          variant="2"
+          onButtonClick={() => ref.current.click()}
+          content="Upload Document"
+        />
+        <input
+          ref={ref}
+          className="form__inputfile"
+          onChange={onHandleFile}
+          type="file"
+          {...rest}
+          multiple
+        />
+      </div>
+    );
+  }
+  return (
+    <input
+      placeholder={placeholder}
+      onChange={onHandleText}
+      className={`form__input-${variant} ${className}`}
+      {...rest}
+    />
+  );
+};
+
+export default Input;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,11 @@
-import React from "react";
+import React from "react";  
 
-import Button from "../../components/atoms/Button";
+import Input from "../../components/atoms/Input";
 
 const Home = () => {
   return (
     <div style={{ padding: "3rem" }}>
-      <Button content="Test" variant="1-1" />
-      <Button content="Test" variant="1-2" />
-      <div>
-        <Button content="Test" variant="1-3" />
-      </div>
-      <div>
-        <Button content="Test" variant="1-4" />
-      </div>
-      <div>
-        <Button content="" icon="Search" variant="2" />
-      </div>
-      <div>
-        <Button content="" icon="ArrowDown" variant="3" />
-      </div>
-      <div>
-        <Button content="Click a photo" icon="Camera" variant="4" />
-      </div>
+      <Input placeholder="Name" variant="1" />
     </div>
   );
 };

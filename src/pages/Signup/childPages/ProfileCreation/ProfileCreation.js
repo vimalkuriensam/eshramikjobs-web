3f8a8210340f6df1146795b07ce20c0e25fe8fb0
unknown <vimalkuriensam@gmail.com>
2021-07-08 11:41:33 +0530

    arrowright icon

--- src/pages/Signup/childPages/ProfileCreation/ProfileCreation.js
+++ src/pages/Signup/childPages/ProfileCreation/ProfileCreation.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-const ProfileCreation = (props) => {
-  console.log(props);
-  return <div>Profile Creation</div>;
+import Title from "../../../../components/atoms/Title";
+
+const ProfileCreation = ({ match }) => {
+  return (
+    <section>
+      <Title variant="pm-26-1" className="u-text-center u-width-100">
+        Create your Eshramik resume
+      </Title>
+    </section>
+  );
 };
 
 export default ProfileCreation;

4e125f5725de85a7fc856302e306a5c89655de8f
unknown <vimalkuriensam@gmail.com>
2021-07-08 07:12:12 +0530

    redux connect

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import AppRoutes from "./routes/appRoutes";
+import { store, persistor } from "./redux/stores/configureStore";
 
 import "./styles/main.scss";
 
 const connectedComponents = (
-  <div>
-    <AppRoutes />
-  </div>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <AppRoutes />
+    </PersistGate>
+  </Provider>
 );
 
 ReactDOM.render(connectedComponents, document.getElementById("app"));

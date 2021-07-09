3fbf21279ecf833023bf9c4e066f0c543ff9cfaf
unknown <vimalkuriensam@gmail.com>
2021-07-09 09:38:50 +0530

    category card

---
+++ src/pages/Home/container/CategoryCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Icon from "../../../components/atoms/Icon";
+import Text from "../../../components/atoms/Text";
+
+const CategoryCard = ({ category }) => {
+  console.log(category);
+  return (
+    <div className="home__card">
+      <Icon name={category.icon} />
+      <Text variant="pl-16-1">{category.title}</Text>
+    </div>
+  );
+};
+
+export default CategoryCard;

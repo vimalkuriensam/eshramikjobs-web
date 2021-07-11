a2143cf3f7cc78339975cdc3fcff196c05e42dca
unknown <vimalkuriensam@gmail.com>
2021-07-11 12:18:40 +0530

    signup states

---
+++ src/pages/Signup/data/index.js
@@ -0,0 +1,11 @@
+export const SIGNUP_ERRORS = {
+  email: "Invalid email format",
+  name: "Should not have spaces or numerals",
+  mobile: "Should be numbers",
+};
+
+export const SIGNUP_DEFAULT_PROPS = {
+  name: "",
+  email: "",
+  mobile: "",
+};

b8db13b3fae3cda8e881ed657698239791636a4a
unknown <vimalkuriensam@gmail.com>
2021-07-11 22:27:18 +0530

    calendar component

--- src/components/atoms/Calendar.js
+++ src/components/atoms/Calendar.js
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 
 import DatePicker from "react-datepicker";
 import moment from "moment";
+import Icon from "./Icon";
 
 const Calendar = () => {
   const [startDate, setStartDate] = useState(moment().valueOf());
   return (
-    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+    <div className="form__calendar">
+      <Icon name="Calendar" />
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
+    </div>
   );
 };
 

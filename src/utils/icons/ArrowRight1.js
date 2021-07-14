757d44a59757e6d9e68d7ce143153ec2406c62e1
unknown <vimalkuriensam@gmail.com>
2021-07-14 10:23:23 +0530

    right arrow 1 icon

---
+++ src/utils/icons/ArrowRight1.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ArrowRight1 = ({ className, style }) => (
+  <svg
+    className={className}
+    style={style}
+    data-name="Layer 1"
+    viewBox="0 0 100 125"
+    x="0px"
+    y="0px"
+  >
+    <title>Arow1-50</title>
+    <polygon points="32.99 97.5 80.49 50 32.99 2.5 19.51 15.98 53.54 50 19.51 84.02 32.99 97.5" />
+  </svg>
+);
+
+export default ArrowRight1;

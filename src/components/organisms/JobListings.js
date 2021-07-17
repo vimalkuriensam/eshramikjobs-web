fe658ba626b39cb7fa4bdab4a05009e905a9a2fa
unknown <vimalkuriensam@gmail.com>
2021-07-17 12:03:33 +0530

    jobs listing ui

--- src/components/organisms/JobListings.js
+++ src/components/organisms/JobListings.js
@@ -1,18 +1,61 @@
 import React from "react";
+import Button from "../atoms/Button";
+import Image from "../atoms/Image";
+import Text from "../atoms/Text";
+import Title from "../atoms/Title";
 
-const JobListings = ({
-  image,
-  title,
-  degree,
-  description,
-  minExperience,
-  maxExperience,
-  location,
-  date,
-}) => {
+const JobListings = ({ jobs = [] }) => {
   return (
     <div>
-      <div>Job Listings</div>
+      {jobs.map(
+        (
+          {
+            image,
+            title,
+            degree,
+            description,
+            minExperience,
+            maxExperience,
+            location,
+            date,
+          },
+          index
+        ) => (
+          <div data-aos="fade-right" className="row jobs" key={index}>
+            <div className="col-25-of-10 u-text-center">
+              <Image className="jobs__imageContainer" name={image} />
+            </div>
+            <div className="col-4-of-10">
+              <Title className="u-margin-vertical-15" variant="pr-19-1">
+                {title}
+              </Title>
+              <Text variant="pl-16-1">{degree}</Text>
+              <Text variant="pl-16-1">{description}</Text>
+              <div>
+                <Text variant="pl-17-2">Experience: </Text>
+                <Text variant="pl-16-1" className="u-margin-bottom-15">
+                  &nbsp;{minExperience} to {maxExperience} years
+                </Text>
+              </div>
+            </div>
+            <div className="col-35-of-10 jobs__cta">
+              <div>
+                <Text variant="pl-17-2">Posted: </Text>
+                <Text variant="pl-16-1">&nbsp;2 hours before</Text>
+              </div>
+              <div>
+                <Text variant="pl-17-2">Location: </Text>
+                <Text variant="pl-16-1">&nbsp;{location}</Text>
+              </div>
+              <Button
+                variant="1-2"
+                className="jobs__apply u-opacity-80"
+                content="Apply"
+              />
+            </div>
+          </div>
+        )
+      )}
     </div>
   );
 };
--- src/pages/Home/container/Samples.js
+++ src/pages/Home/container/Samples.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { connect } from "react-redux";
 import Title from "../../../components/atoms/Title";
 
 import CompanyListings from "../../../components/organisms/CompanyListings";
 import WorkListings from "../../../components/organisms/JobListings";
 
-const Samples = () => {
+const Samples = ({ jobs }) => {
   return (
     <section className="section-home-samples">
       <div className="home__sampleContainer">
         <div className="home__sampleJobs">
-          <Title variant="pr-30-1">Recent jobs</Title>
-          <WorkListings />
+          <Title className="u-margin-bottom-50" variant="pr-30-1">
+            Recent jobs
+          </Title>
+          <WorkListings jobs={jobs} />
         </div>
         <div className="home__sampleCompany">
           <Title variant="pr-30-1">Companies</Title>
@@ -21,4 +24,8 @@ const Samples = () => {
   );
 };
 
-export default Samples;
+const mapStateToProps = (state) => ({
+  jobs: state.jobs.recent,
+});
+
+export default connect(mapStateToProps)(Samples);

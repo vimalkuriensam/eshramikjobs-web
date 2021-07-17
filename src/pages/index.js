01a0c0ff1cc2a679865b2d3462d6444dbf73b162
unknown <vimalkuriensam@gmail.com>
2021-07-17 18:50:10 +0530

    about us component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Signup, ProfileCreation, OTP } from "./Signup";
-export { default as Jobs, Applied, Recommended, Saved } from './Jobs';
+export { default as Jobs, Applied, Recommended, Saved } from "./Jobs";
+export { default as AboutUs } from "./AboutUs";
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -8,7 +8,7 @@ import Header from "../components/organisms/Header";
 import Login from "../components/organisms/Login";
 import Loader from "../components/organisms/Loader";
 import Footer from "../components/organisms/Footer";
-import { Home as HomeView } from "../pages";
+import { Home as HomeView, AboutUs as AboutUsView } from "../pages";
 
 import { RegisterChildView } from "./childRoutes/Register";
 import { JobsChildView } from "./childRoutes/Jobs";
@@ -26,6 +26,7 @@ const AppRoutes = () => (
       <Route path="/home" component={HomeView} />
       <Route path="/register" component={RegisterChildView} />
       <Route path="/jobs" component={JobsChildView} />
+      <Route path="/about" component={AboutUsView} />
     </Switch>
     <Footer />
   </Router>

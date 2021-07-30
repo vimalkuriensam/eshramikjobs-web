23d671f32cbdcfe753bbd19331c4d7da1f9e8509
unknown <vimalkuriensam@gmail.com>
2021-07-30 17:13:20 +0530

    grid icon

---
+++ src/utils/icons/Grid.js
@@ -0,0 +1,61 @@
+import react from "react";
+
+const Grid = ({ className, style }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="19"
+    height="18"
+    viewBox="0 0 19 18"
+    className={className}
+    style={style}
+  >
+    <g id="Group_65" data-name="Group 65" transform="translate(-1194 -828)">
+      <g
+        id="Rectangle_28"
+        data-name="Rectangle 28"
+        transform="translate(1194 828)"
+        fill="none"
+        stroke="#126353"
+        stroke-width="1"
+      >
+        <rect width="8" height="8" rx="2" stroke="none" />
+        <rect x="0.5" y="0.5" width="7" height="7" rx="1.5" fill="none" />
+      </g>
+      <g
+        id="Rectangle_29"
+        data-name="Rectangle 29"
+        transform="translate(1205 828)"
+        fill="none"
+        stroke="#126353"
+        stroke-width="1"
+      >
+        <rect width="8" height="8" rx="2" stroke="none" />
+        <rect x="0.5" y="0.5" width="7" height="7" rx="1.5" fill="none" />
+      </g>
+      <g
+        id="Rectangle_30"
+        data-name="Rectangle 30"
+        transform="translate(1194 838)"
+        fill="none"
+        stroke="#126353"
+        stroke-width="1"
+      >
+        <rect width="8" height="8" rx="2" stroke="none" />
+        <rect x="0.5" y="0.5" width="7" height="7" rx="1.5" fill="none" />
+      </g>
+      <g
+        id="Rectangle_31"
+        data-name="Rectangle 31"
+        transform="translate(1205 838)"
+        fill="none"
+        stroke="#126353"
+        stroke-width="1"
+      >
+        <rect width="8" height="8" rx="2" stroke="none" />
+        <rect x="0.5" y="0.5" width="7" height="7" rx="1.5" fill="none" />
+      </g>
+    </g>
+  </svg>
+);
+
+export default Grid;

da1eb511347c573cb339127c320df5abbaa7b6d3
unknown <vimalkuriensam@gmail.com>
2021-08-12 23:03:41 +0530

    environment var refactor #6

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,10 +9,16 @@ module.exports = (env) => {
   const fileEnv = dotenv.config({
     path: path.join(__dirname, `/src/environment/.env.${env}`),
   }).parsed;
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
-    return prev;
-  }, {});
+  // const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  //   prev[`process.env.${next}`] = JSON.stringify(process.env[next]);
+  //   return prev;
+  // }, {});
+  const envKeys = {
+    "process.env": {
+      BASE_URL: process.env.BASE_URL,
+      RAZORPAY_ID: process.env.RAZORPAY_ID,
+    },
+  };
   console.log(envKeys);
   return merge(common, {
     mode: "production",

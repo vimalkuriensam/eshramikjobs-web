689289e0f154647ccc4c7e9bffc01209e9734701
unknown <vimalkuriensam@gmail.com>
2021-08-12 22:41:22 +0530

    environment var refactor #1

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     path: path.join(__dirname, `/src/environment/.env.${env}`),
   }).parsed;
   const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = `process.env.${next}`;
+    prev[`process.env.${next}`] = JSON.stringify(dotenv.config().parsed);
     return prev;
   }, {});
   console.log(envKeys);

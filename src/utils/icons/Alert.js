2e18dfb5ee28a8238e7e1b10c1886daff0412e10
unknown <vimalkuriensam@gmail.com>
2021-08-14 18:22:07 +0530

    alert icon

---
+++ src/utils/icons/Alert.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Alert = ({ className, style }) => (
+  <svg
+    space="preserve"
+    version="1.1"
+    style={{
+      shapeRendering: "geometricPrecision",
+      textRendering: "geometricPrecision",
+      imageRendering: "optimizeQuality",
+      ...style,
+    }}
+    className={className}
+    viewBox="0 0 846.66 1058.325"
+    x="0px"
+    y="0px"
+    fillRule="evenodd"
+    clipRule="evenodd"
+  >
+    <g>
+      <path
+        class="fil0"
+        style={{ fill: "black" }}
+        d="M717.05 690.58c19.54,0 26.66,-24.39 17.03,-40.87l-279.56 -478.54c-11.75,-20.12 -50.64,-20.12 -62.39,0l-279.56 478.54c-9.63,16.48 -2.51,40.87 17.04,40.87l587.44 0zm-261.66 -374.68l0 197.7 -64.12 0 0 -197.7 64.12 0zm0 237.77l0 60.78 -64.12 0 0 -60.78 64.12 0z"
+      />
+    </g>
+  </svg>
+);
+
+export default Alert;
